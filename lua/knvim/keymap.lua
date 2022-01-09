b761d718c469636187411c9ca4cddc00f1a84c48
Saad Parwaiz <saad.parwaiz1@gmail.com>
2022-01-09 10:23:23 +0000

    refactor(keymap): use new lua keymap api

--- lua/knvim/keymap.lua
+++ lua/knvim/keymap.lua
@@ -4,43 +4,23 @@ local keymap = {}
 
 local __temp_map = {}
 
-local map = vim.api.nvim_set_keymap
-local bmap = vim.api.nvim_buf_set_keymap
-
 -- Map Lua Functions and Strings
 --- @param lhs string
 --- @param rhs function|string
 --- @param opts table
 function keymap.map(lhs, rhs, opts)
 	opts = opts or {}
-	local create_map
-	if opts.buffer then
-		create_map = function(m, l, r, o)
-			bmap(opts.buffer, m, l, r, o)
-		end
-	else
-		create_map = map
-	end
 	local options = {
 		expr = opts.expr,
 		nowait = opts.nowait,
 		script = opts.script,
 		unique = opts.unique,
 		silent = opts.silent == nil and true or (opts.silent or false),
-		noremap = opts.noremap == nil and true or (opts.silent or false),
+		remap = not (opts.noremap == nil and true or (opts.noremap or false)),
+    buffer = opts.buffer
 	}
 	opts.mode = opts.mode or "n"
-	if vim.is_callable(rhs) then
-		options.callback = rhs
-		rhs = ""
-	end
-	if type(opts.mode) == "table" then
-		for _, v in pairs(opts.mode) do
-			create_map(v, lhs, rhs, options)
-		end
-	else
-		create_map(opts.mode, lhs, rhs, options)
-	end
+  vim.keymap.set(opts.mode, lhs, rhs, options)
 end
 
 -- Delete Maps
@@ -48,23 +28,7 @@ end
 ---@param mode string
 ---@param buffer number
 function keymap.del(lhs, mode, buffer)
-	mode = mode or "n"
-	local del_map
-	if buffer then
-		del_map = function(m, l)
-			vim.api.nvim_buf_del_keymap(buffer, m, l)
-		end
-	else
-		del_map = vim.api.nvim_del_keymap
-	end
-
-	if type(mode) == "table" then
-		for _, v in pairs(mode) do
-			del_map(v, lhs)
-		end
-	else
-		del_map(mode, lhs)
-	end
+  vim.keymap.del(mode or "n", lhs, { buffer = buffer })
 end
 
 function keymap.tmp(lhs, rhs, opts)
@@ -72,7 +36,7 @@ function keymap.tmp(lhs, rhs, opts)
 	opts.buffer = opts.buffer or 0
 	local mode = opts.mode or "n"
 	local d_rhs = vim.tbl_filter(function (x)
-	  return x['lhs'] == lhs
+	  return x['lhs'] == lhs:gsub('<leader>', vim.g.mapleader)
 	end, vim.api.nvim_buf_get_keymap(opts.buffer, mode))
   d_rhs = d_rhs or {}
   d_rhs = d_rhs[1] or {}
